ae6bf84f95d38b7436abae5586b62d3537bfde90
linuspauling1 <paulb2233@yahoo.com>
2024-02-04 10:26:34 +0200

    Played around wth shadow boxes and background colours

--- app.py
+++ app.py
@@ -38,5 +38,4 @@ def delete_todo(todo_id):
 
 if __name__ == '__main__':
     db.create_all()
-    #app.run(host='0.0.0.0',port=5000)
-    serve(app,host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000)
